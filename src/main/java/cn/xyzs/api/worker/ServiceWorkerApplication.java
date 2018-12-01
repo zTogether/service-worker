7fa7d4a6b2f2c014717f2e9b388ef55220e81365
zTogether <zhouucchao@163.com>
2018-12-01 10:54:28 +0800

    工人模块，服务提供者(二次封装)

--- src/main/java/cn/xyzs/api/worker/ServiceWorkerApplication.java
+++ src/main/java/cn/xyzs/api/worker/ServiceWorkerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
 import java.util.Properties;
